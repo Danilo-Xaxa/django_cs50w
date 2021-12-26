8304f20a288d8e4a9b84fd3a50095b7a70b486cd
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-26 15:18:39 -0300

    adicionando css e bootstrap

--- aula3/anonovo/views.py
+++ aula3/anonovo/views.py
@@ -4,5 +4,5 @@ from datetime import date
 
 # Create your views here.
 def index(request):
-    d = 'date.today().strftime("%d/%m/%Y")'
+    d = "date.today().strftime('%d/%m/%Y')"
     return render(request, 'anonovo/index.html', {"resposta": "Sim!" if d.startswith('01/01') else "Não."})
