e536ef06e221d724ed4975e7e8bfdd6551a40866
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-26 23:43:48 -0300

    separate user sessions

--- aula3/tarefas/views.py
+++ aula3/tarefas/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-
-tarefas = ["foo", "bar", "baz"]
 
 class NovaTarefaForm(forms.Form):
     nome_tarefa = forms.CharField(label='Nome Tarefa', max_length=100)
@@ -13,15 +13,17 @@ class NovaTarefaForm(forms.Form):
 
 # Create your views here.
 def index(request):
-    return render(request, 'tarefas/index.html', {'tarefas': tarefas})
+    if "tarefas" not in request.session:
+        request.session["tarefas"] = []
+    return render(request, 'tarefas/index.html', {'tarefas': request.session["tarefas"]})
 
 def adicionar(request):
     if request.method == 'POST':
         form = NovaTarefaForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['nome_tarefa'])
-            tarefas.append(form.cleaned_data['nome_tarefa'])
-            return render(request, 'tarefas/index.html', {'tarefas': tarefas})
+            tarefa = form.cleaned_data['nome_tarefa']
+            request.session["tarefas"] += [tarefa]
+            return HttpResponseRedirect(reverse('tarefas:index'))
         else:
             return render(request, 'tarefas/adicionar.html', {'form': form})
 
